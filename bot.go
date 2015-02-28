745fafc8ed2cb39c5baa57cac4f55c3a4e32bb5a
Merlyn <curious.attempt.bunny@gmail.com>
2015-02-27 21:53:23 -0800

    Picks the first option. Makes no moves.

--- bot.go
+++ bot.go
@@ -10,6 +10,8 @@ func main() {
 
     for {
         line, err := reader.ReadString('\n')
+        // our_name := ""
+        // their_name := ""
 
         if err != nil {
             break
@@ -24,11 +26,16 @@ func main() {
         } else if strings.Index(line, "#") == 0 {
 
         } else if parts[0] == "settings" {
-
+            if parts[1] == "your_bot" {
+                // our_name = parts[2]
+            } else if parts[1] == "opponent_bot" {
+                // their_name = parts[2]
+            }
         } else if parts[0] == "setup_map" {
 
         } else if parts[0] == "pick_starting_region" {
-
+            // pick the first one
+            fmt.Println(parts[2])
         } else if parts[0] == "update_map" {
 
         } else if parts[0] == "opponent_moves" {
@@ -38,9 +45,9 @@ func main() {
         } else if strings.Index(line, "Output from your bot: ") == 0 {
 
         } else if strings.Index(line, "go place_armies") == 0 {
-
+            fmt.Println("No moves")
         } else if strings.Index(line, "go attack/transfer") == 0 {
-
+            fmt.Println("No moves")
         } else {
             fmt.Fprintf(os.Stderr, "Don't recognire: "+line+"\n")
         }
---
+++ package.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+zip bot.zip *.go
