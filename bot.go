2364a9acae9e293da1a7d6374b83416c5145d980
Merlyn <curious.attempt.bunny@gmail.com>
2015-02-27 21:28:50 -0800

    Recognize all possible command stems.

--- bot.go
+++ bot.go
@@ -3,6 +3,7 @@ package main
 import "os"
 import "fmt"
 import "bufio"
+import "strings"
 
 func main() {
     reader := bufio.NewReader(os.Stdin)
@@ -14,6 +15,34 @@ func main() {
             break
         }
 
-        fmt.Printf("<< "+line)
+        line = strings.TrimSpace(line)
+
+        parts := strings.Split(line, " ")
+
+        if len(line) == 0 {
+
+        } else if strings.Index(line, "#") == 0 {
+
+        } else if parts[0] == "settings" {
+
+        } else if parts[0] == "setup_map" {
+
+        } else if parts[0] == "pick_starting_region" {
+
+        } else if parts[0] == "update_map" {
+
+        } else if parts[0] == "opponent_moves" {
+
+        } else if parts[0] == "Round" {
+
+        } else if strings.Index(line, "Output from your bot: ") == 0 {
+
+        } else if strings.Index(line, "go place_armies") == 0 {
+
+        } else if strings.Index(line, "go attack/transfer") == 0 {
+
+        } else {
+            fmt.Fprintf(os.Stderr, "Don't recognire: "+line+"\n")
+        }
     }
 }
