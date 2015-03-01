c10520b40228312c75a5432453bd0ec53e9ac72d
Merlyn Albery-Speyer <merlyn@newrelic.com>
2015-02-28 19:21:50 -0800

    Add reinforcement logic.

--- bot.go
+++ bot.go
@@ -157,6 +157,42 @@ func main() {
         } else if strings.Index(line, "go attack/transfer") == 0 {
             fmt.Fprintf(os.Stderr, "Attacks:\n")
             attacks := []Attack{}
+
+            move_to := ourBorderRegions(state)
+            considered := make(map[int64]bool)
+            for _, region := range move_to {
+                considered[region.id] = true
+            }
+
+            for {
+                if len(move_to) == 0 {
+                    break
+                }
+
+                region := move_to[0]
+                move_to = move_to[1:]
+
+                for _, neighbour := range region.neighbours {
+                    if neighbour.owner != "us" {
+                        continue
+                    }
+
+                    if considered[neighbour.id] {
+                        continue
+                    }
+
+                    if neighbour.armies > 1 {
+                        attacks = append(attacks, Attack{
+                            region_from: neighbour,
+                            region_to: region,
+                            armies: neighbour.armies - 1})
+                    }
+
+                    considered[neighbour.id] = true
+                    move_to = append(move_to, neighbour)
+                }
+            }
+
             used := make(map[int64]bool)
             for {
                 var attack_from *Region
