0819e83cd42e0ea98cd91d115b19d9b4bd1cd09e
Merlyn Albery-Speyer <merlyn@newrelic.com>
2015-03-07 10:52:28 -0800

    Restructuring code.

--- attack.go
+++ attack.go
@@ -1,5 +1,48 @@
 package main
 
+func reinforcement_moves(state *State, attacks []Attack) []Attack {
+    move_to := ourBorderRegions(state)
+    considered := make(map[int64]bool)
+    for _, region := range move_to {
+        considered[region.id] = true
+    }
+
+    for {
+        if len(move_to) == 0 {
+            break
+        }
+
+        region := move_to[0]
+        move_to = move_to[1:]
+
+        for _, neighbour := range region.neighbours {
+            if neighbour.owner != "us" {
+                continue
+            }
+
+            if considered[neighbour.id] {
+                continue
+            }
+
+            if neighbour.armies > 1 {
+                attacks = append(attacks, Attack{
+                    region_from: neighbour,
+                    region_to: region,
+                    armies: neighbour.armies - 1})
+            }
+
+            considered[neighbour.id] = true
+            move_to = append(move_to, neighbour)
+        }
+    }
+
+    return attacks
+}
+
+//
+//      attack_moves
+//
+
 func attack_moves(state *State, attacks []Attack) ([]Attack, map[int64]bool) {
       superRegionOwnedByEnemy := make(map[int64]bool)
     for _, super_region := range state.super_regions {
@@ -153,4 +196,86 @@ func attack_moves(state *State, attacks []Attack) ([]Attack, map[int64]bool) {
     }
 
     return attacks, newly_captured
+}
+
+//
+//   border_reinforcement_moves
+//
+
+func border_reinforcement_moves(state *State, attacks []Attack, newly_captured map[int64]bool) []Attack {
+    ideal_destinations := ourBorderRegionsWithTheEnemy(state)
+    regionToIdeal := make(map[int64]bool)
+    for _, region := range ideal_destinations {
+        regionToIdeal[region.id] = true
+    }
+
+    move_from := ourBorderRegionsWithNeutralOnly(state)
+    armiesPlaced := make(map[int64]int64)
+    for _, region := range move_from {
+        armiesPlaced[region.id] = region.armies
+    }
+
+    for _, region := range move_from {
+        if region.armies == 1 {
+            continue
+        }
+
+        if newly_captured[region.id] {
+            continue
+        }
+
+        if armiesPlaced[region.id] > region.armies {
+            continue // don't move armies if we've been reinforced
+        }
+
+        var best_destination *Region
+        best_destination = nil
+
+        for _, neighbour := range region.neighbours {
+            if neighbour.owner != "us" {
+                continue
+            }
+
+            if best_destination == nil ||
+                (armiesPlaced[neighbour.id] >= region.armies &&
+                    regionToIdeal[neighbour.id] == regionToIdeal[region.id]) ||
+                (regionToIdeal[neighbour.id] && !regionToIdeal[region.id]) {
+                borders_something := false
+                for _, n := range neighbour.neighbours {
+                    if n.owner != "us" {
+                        borders_something = true
+                        break
+                    }
+                }
+
+                if borders_something {
+                    best_destination = neighbour
+                }
+            }
+        }
+
+        if best_destination != nil {
+            armies_to_move := region.armies - 1
+            safe_to_prepend := (newly_captured[best_destination.id] != true)
+
+            attack := Attack{
+                        region_from: region,
+                        region_to: best_destination,
+                        armies: armies_to_move }
+            if safe_to_prepend {
+                new_attacks := make([]Attack, len(attacks)+1)
+                copy(new_attacks[1:], attacks[:])
+                new_attacks[0] = attack
+                attacks = new_attacks
+            } else {
+                attacks = append(attacks, attack)
+            }
+
+            region.armies -= armies_to_move
+            armiesPlaced[best_destination.id] += armies_to_move
+
+        }
+    }
+
+    return attacks
 }
--- bot.go
+++ bot.go
@@ -469,3 +469,16 @@ type Attack struct {
     region_to *Region
     armies int64
 }
+
+func calculateAttacks(state *State) []Attack {
+    attacks := []Attack{}
+
+    attacks = reinforcement_moves(state, attacks)
+
+    var newly_captured map[int64]bool
+    attacks, newly_captured = attack_moves(state, attacks)
+
+    attacks = border_reinforcement_moves(state, attacks, newly_captured)
+
+    return attacks
+}
